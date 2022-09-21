5c7219bb91276bd6cd643a8a1cbb96c43406030d
muratyarali <murat@naxtech.dev>
2022-09-21 11:29:50 +0300

    main.tf files created

---
+++ part2-infra-detailed/main.tf
@@ -0,0 +1,156 @@
+############################
+# PROVIDER
+############################
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.31.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+############################
+#  VPC MODULE
+############################
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "eks-cluster-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_dns_hostnames = true # Should be true to enable DNS hostnames in the VPC
+  enable_dns_support   = true # Should be true to enable DNS support in the VPC
+  enable_nat_gateway   = false
+
+  tags = {
+    Terraform   = "true"
+    Environment = "devops"
+  }
+}
+
+############################
+# EKS ROLE
+############################
+
+resource "aws_iam_role" "eks-iam-role" {
+  name = "devopsthehardway-eks-iam-role"
+
+  path = "/"
+
+  assume_role_policy = <<EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "eks.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
+      }
+    ]
+  }
+EOF
+
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks-iam-role.name
+}
+resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly-EKS" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks-iam-role.name
+}
+
+############################
+# EKS CLUSTER
+############################
+
+resource "aws_eks_cluster" "devopsthehardway-eks" {
+  name     = "devopsthehardway-cluster"
+  role_arn = aws_iam_role.eks-iam-role.arn
+
+  vpc_config {
+    subnet_ids = [element(module.vpc.public_subnets, 0), element(module.vpc.public_subnets, 1)]
+  }
+
+  depends_on = [
+    aws_iam_role.eks-iam-role,
+    module.vpc
+  ]
+}
+
+############################
+# WORKER NODES ROLE
+############################
+
+resource "aws_iam_role" "workernodes" {
+  name = "eks-node-group-example"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.workernodes.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.workernodes.name
+}
+
+resource "aws_iam_role_policy_attachment" "EC2InstanceProfileForImageBuilderECRContainerBuilds" {
+  policy_arn = "arn:aws:iam::aws:policy/EC2InstanceProfileForImageBuilderECRContainerBuilds"
+  role       = aws_iam_role.workernodes.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.workernodes.name
+}
+
+############################
+# WORKER NODES
+############################
+
+resource "aws_eks_node_group" "worker-node-group" {
+  cluster_name    = aws_eks_cluster.devopsthehardway-eks.name
+  node_group_name = "devopsthehardway-workernodes"
+  node_role_arn   = aws_iam_role.workernodes.arn
+  subnet_ids      = [element(module.vpc.public_subnets, 0), element(module.vpc.public_subnets, 0)]
+  instance_types  = ["t2.micro"]
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 1
+    min_size     = 1
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
+    #aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
+  ]
+}
---
+++ part2-infra-module/main.tf
@@ -0,0 +1,174 @@
+############################
+# PROVIDER
+############################
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.31.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+############################
+#  VPC MODULE
+############################
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "eks-cluster-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_dns_hostnames = true # Should be true to enable DNS hostnames in the VPC
+  enable_dns_support   = true # Should be true to enable DNS support in the VPC
+  enable_nat_gateway   = false
+
+  tags = {
+    Terraform   = "true"
+    Environment = "devops"
+  }
+}
+
+############################
+# EKS MODULE
+############################
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 18.0"
+
+  cluster_name    = "my-cluster"
+  cluster_version = "1.22"
+
+  cluster_endpoint_private_access = true
+  cluster_endpoint_public_access  = true
+
+  cluster_addons = {
+    coredns = {
+      resolve_conflicts = "OVERWRITE"
+    }
+    kube-proxy = {}
+    vpc-cni = {
+      resolve_conflicts = "OVERWRITE"
+    }
+  }
+
+  # cluster_encryption_config = [{
+  #   provider_key_arn = "arn:aws:kms:eu-west-1:111122223333:key/1234abcd-12ab-34cd-56ef-1234567890ab"
+  #   resources        = ["secrets"]
+  # }]
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = [element(module.vpc.public_subnets, 0), element(module.vpc.public_subnets, 1)]
+
+  # # Self Managed Node Group(s)
+  # self_managed_node_group_defaults = {
+  #   instance_type                          = "m6i.large"
+  #   update_launch_template_default_version = true
+  #   iam_role_additional_policies = [
+  #     "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  #   ]
+  # }
+
+  # self_managed_node_groups = {
+  #   one = {
+  #     name         = "mixed-1"
+  #     max_size     = 5
+  #     desired_size = 2
+
+  #     use_mixed_instances_policy = true
+  #     mixed_instances_policy = {
+  #       instances_distribution = {
+  #         on_demand_base_capacity                  = 0
+  #         on_demand_percentage_above_base_capacity = 10
+  #         spot_allocation_strategy                 = "capacity-optimized"
+  #       }
+
+  #       override = [
+  #         {
+  #           instance_type     = "m5.large"
+  #           weighted_capacity = "1"
+  #         },
+  #         {
+  #           instance_type     = "m6i.large"
+  #           weighted_capacity = "2"
+  #         },
+  #       ]
+  #     }
+  #   }
+  # }
+
+  # EKS Managed Node Group(s)
+  eks_managed_node_group_defaults = {
+    disk_size      = 30
+    instance_types = ["t2.micro"]
+  }
+
+  eks_managed_node_groups = {
+    # blue = {}
+    green = {
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+
+      instance_types = ["t2.micro"]
+      capacity_type  = "ON_DEMAND"
+    }
+  }
+
+  # # Fargate Profile(s)
+  # fargate_profiles = {
+  #   default = {
+  #     name = "default"
+  #     selectors = [
+  #       {
+  #         namespace = "default"
+  #       }
+  #     ]
+  #   }
+  # }
+
+  # # aws-auth configmap
+  # manage_aws_auth_configmap = true
+
+  # aws_auth_roles = [
+  #   {
+  #     rolearn  = "arn:aws:iam::66666666666:role/role1"
+  #     username = "role1"
+  #     groups   = ["system:masters"]
+  #   },
+  # ]
+
+  # aws_auth_users = [
+  #   {
+  #     userarn  = "arn:aws:iam::66666666666:user/user1"
+  #     username = "user1"
+  #     groups   = ["system:masters"]
+  #   },
+  #   {
+  #     userarn  = "arn:aws:iam::66666666666:user/user2"
+  #     username = "user2"
+  #     groups   = ["system:masters"]
+  #   },
+  # ]
+
+  # aws_auth_accounts = [
+  #   "777777777777",
+  #   "888888888888",
+  # ]
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
