d9e4288c4695d3e6df7940f0499caf96efdbcda3
muratyarali <murat@naxtech.dev>
2022-09-27 17:12:58 +0300

    hello and welcome app updated

--- part2-src/welcome-nax/welcome.py
+++ part2-src/welcome-nax/welcome.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def welcome():
-    return "<h1>Welcome to Nax</h1>"
+    return "<h1>Welcome to Nax Tech</h1>"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
